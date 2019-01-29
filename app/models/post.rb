827c1be4c7b7afe3d201a3010389aa26d5aa5e52
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2019-01-29 16:52:01 +0400

    Create vote

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :votes, dependent: :destroy
 end
--- db/migrate/20190129122119_create_posts.rb
+++ db/migrate/20190129122119_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.string :author_ip
+      t.inet :author_ip
       t.integer :author_id, index: true
 
       t.timestamps
