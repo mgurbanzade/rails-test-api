07a39fae89332e2b769ec1399c79eda88ee77482
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2019-02-01 17:20:31 +0400

    Queries performance optimization

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,20 +5,23 @@ class Post < ApplicationRecord
   validates_presence_of :title, :body
 
   def self.top_posts
-    Post
-      .where('id IN (SELECT DISTINCT(post_id) FROM votes)')
-      .sort_by(&:average_rating)
+    Post.joins(:votes)
+        .select("posts.*, AVG(votes.value) AS average_ratings")
+        .group("posts.id")
+        .order('average_ratings DESC')
   end
 
   def self.ip_list
-    list = Post.select(:author_ip).group(:author_ip).having("COUNT(author_ip) > 1")
-            .map { |p| p.author_ip.to_s }.to_a
+    list = Post.includes(:author)
+               .where(author_ip: Post.select(:author_ip)
+                                     .group(:author_ip)
+                                     .having("COUNT(author_ip) > 1"))
 
-    list.map { |ip| { ip => Post.where(author_ip: ip).collect(&:author).collect(&:login)} }
-  end
+    users = User.all.collect(&:login)
+    duplicates = list.map { |p| { p.author_ip.to_s => users.select { |u| u == p.author.login } } }
 
-  def average_rating
-    votes = self.reload.votes.map { |v| v.value }
-    (votes.sum / votes.size.to_f).round(1)
+    duplicates.flat_map(&:entries)
+              .group_by(&:first)
+              .map { |ip, arr| Hash[ip, arr.map(&:last).flatten ] }
   end
 end
